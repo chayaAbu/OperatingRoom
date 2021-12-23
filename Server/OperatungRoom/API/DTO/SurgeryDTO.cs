e11632b8a5aaf207566ad8dbe82076fd86d8160f
chayaAbu <chaya.s.a8@gmail.com>
2021-12-23 16:08:57 +0200

    3

---
+++ Server/OperatungRoom/API/DTO/SurgeryDTO.cs
@@ -0,0 +1,47 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.DTO
+{
+    public class SurgeryDTO
+    {
+        public int surgeryCode { get; set; }
+        public int idClass { get; set; }
+        public int priorityLevel { get; set; }
+        public int dangerLevel { get; set; }
+        public int idDoctor { get; set; }
+        public DateTime surgeryDate { get; set; }
+
+        public SurgeryDTO()
+        {
+                
+        }
+
+        public SurgeryDTO(surgery s)
+        {
+            this.surgeryCode = s.surgeryCode;
+            this.idClass = s.idClass;
+            this.priorityLevel = s.priorityLevel;
+            this.dangerLevel = s.dangerLevel;
+            this.idDoctor = s.idDoctor;
+            this.surgeryDate = s.surgeryDate;
+
+        }
+        public static List<SurgeryDTO> CreateSurgeryDtoListg(List<surgery> surgeryList)
+        {
+            List<SurgeryDTO> dtoSurgeryList = new List<SurgeryDTO>();
+            foreach (var s in surgeryList)
+            {
+                SurgeryDTO dtoSurgery = new SurgeryDTO(s);
+                dtoSurgeryList.Add(dtoSurgery);
+            }
+            return dtoSurgeryList;
+        }
+
+    }
+    
+ 
+}
