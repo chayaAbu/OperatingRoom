ec610eb537f970776940c4ff1fcd5b2e4a81c0ab
chayaAbu <chaya.s.a8@gmail.com>
2022-01-11 10:24:14 +0200

    1

---
+++ Server/OperatungRoom/BL/SurgeryManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.DTO;
+using DAL;
+
+
+namespace BL
+{
+
+
+    class SurgeryManager
+    {
+        static DBConection db = new DBConection();
+        public static List<SurgeryDTO>
+    }
+}
