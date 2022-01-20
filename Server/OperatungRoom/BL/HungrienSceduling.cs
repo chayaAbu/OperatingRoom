504036326f12ea815cc9bdd504053bb4a99d9336
chayaAbu <chaya.s.a8@gmail.com>
2022-01-20 17:15:07 +0200

    20/01/22 2

---
+++ Server/OperatungRoom/BL/HungrienSceduling.cs
@@ -0,0 +1,21 @@
+﻿using API.DTO;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+   public class HungrienSceduling
+    {
+      int[][] surgeryMatrix;
+        
+        public int CalculateScore(SurgeryDTO surgery)
+        {
+            return 0;
+        }
+    
+    }
+}
